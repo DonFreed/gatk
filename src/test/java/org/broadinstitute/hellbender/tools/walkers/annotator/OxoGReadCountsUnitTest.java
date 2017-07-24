8185797bb6e51606a7410e6952aa072926a0a142
James <emeryj@broadinstitute.org>
2017-07-24 11:09:29 -0400

    In progress work on the variant annotation framework looking to provide minimal functionality ported

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/annotator/OxoGReadCountsUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/annotator/OxoGReadCountsUnitTest.java
@@ -1,10 +1,8 @@
 package org.broadinstitute.hellbender.tools.walkers.annotator;
 
-import com.google.common.collect.ImmutableMap;
 import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.TextCigarCodec;
 import htsjdk.variant.variantcontext.*;
-import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.broadinstitute.hellbender.utils.QualityUtils;
@@ -20,7 +18,6 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -133,7 +130,7 @@ public final class OxoGReadCountsUnitTest {
         badRead.setMappingQuality(20);
 
         final ReadLikelihoods<Allele> likelihoods =
-                AnnotationArtificialData.makeLikelihoods(sample1, refReads, altReads, Arrays.asList(badRead), -100.0, -10.0, -1.1, refAllele, altAllele);
+                ArtificialAnnotationUtils.makeLikelihoods(sample1, refReads, altReads, Arrays.asList(badRead), -100.0, -10.0, -1.1, refAllele, altAllele);
 
         return ImmutablePair.of(new VariantContextBuilder("test", "20", 10, 10, alleles).genotypes(Arrays.asList(g)).make(),
                 likelihoods);
